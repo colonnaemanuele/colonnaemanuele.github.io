b3cf1124450b56434931a8f4320a81c98672a27e
Emanuele Colonna <colonnamanu98@gmail.com>
2025-04-22 15:17:39 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   app: {
     baseURL: "/",
-    buildAssetsDir: '/_nuxt/',
+    buildAssetsDir: "/_nuxt/",
     head: {
       title: "My Nuxt App",
       meta: [{ name: "description", content: "My Nuxt App Description" }],
