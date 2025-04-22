6e09d1ec1ed9eae83100c7d1eb4452f3ce613c37
Emanuele Colonna <colonnamanu98@gmail.com>
2025-04-22 13:36:32 +0200

    Deploy Nuxt to GitHub Pages

---
+++ .nuxt/dist/server/_virtual/virtual_nuxt__Users_emanuele_Workspace_colonnaemanuele.github.io_.nuxt_middleware.mjs
@@ -0,0 +1,8 @@
+import manifest_45route_45rule from "../node_modules/nuxt/dist/app/middleware/manifest-route-rule.mjs";
+const globalMiddleware = [
+  manifest_45route_45rule
+];
+export {
+  globalMiddleware
+};
+//# sourceMappingURL=virtual_nuxt__Users_emanuele_Workspace_colonnaemanuele.github.io_.nuxt_middleware.mjs.map
---
+++ .nuxt/dist/server/_virtual/virtual_nuxt__Users_emanuele_Workspace_colonnaemanuele.github.io_.nuxt_nuxt.config.mjs
@@ -0,0 +1,11 @@
+const nuxtLinkDefaults = { "componentName": "NuxtLink" };
+const asyncDataDefaults = { "value": null, "errorValue": null, "deep": true };
+const nuxtDefaultErrorValue = null;
+const appId = "nuxt-app";
+export {
+  appId,
+  asyncDataDefaults,
+  nuxtDefaultErrorValue,
+  nuxtLinkDefaults
+};
+//# sourceMappingURL=virtual_nuxt__Users_emanuele_Workspace_colonnaemanuele.github.io_.nuxt_nuxt.config.mjs.map
